8a1d2d6d1c7bd88f94466eb158a28e29dcac1d34
evo264714 <mh264714@gmail.com>
2023-04-06 22:07:58 +0600

    second

--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,8 @@ import Home from './components/Home/Home';
 import First from './components/First/First';
 import Friends from './components/Friends/Friends';
 import FriendDetail from './components/FriendDetail/FriendDetail';
+import Posts from './components/Posts/Posts';
+import PostDetails from './components/PostDetails/PostDetails';
 
 // const router = createBrowserRouter([
 //   {
@@ -44,7 +46,18 @@ const router = createBrowserRouter([
       },
       {
         path: 'friend/:friendId',
-        element: <FriendDetail></FriendDetail>
+        element: <FriendDetail></FriendDetail>,
+        loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${params.friendId}`)
+      },
+      {
+        path: 'posts',
+        element: <Posts></Posts>,
+        loader: () => fetch('https://jsonplaceholder.typicode.com/posts')
+      },
+      {
+        path: 'post/:postId',
+        element: <PostDetails></PostDetails>,
+        loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
       },
       {
         path: 'about',
