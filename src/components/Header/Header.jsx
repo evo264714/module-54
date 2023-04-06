dd4ee27a134fb0dd6e2ee76b7c2f22f7768dfab5
evo264714 <mh264714@gmail.com>
2023-04-06 22:28:05 +0600

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header =  () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to='/posts'>Posts</Link>
-            <Link to='/friends'>Friends</Link>
+            <ActiveLink to="/">Home</ActiveLink>
+            <ActiveLink to="/about">About</ActiveLink>
+            <ActiveLink to="/contact">Contact</ActiveLink>
+            <ActiveLink to='/posts'>Posts</ActiveLink>
+            <ActiveLink to='/friends'>Friends</ActiveLink>
         </nav>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -66,6 +66,10 @@ const router = createBrowserRouter([
       {
         path: 'contact',
         element: <Contact></Contact>
+      },
+      {
+        path: '*',
+        element: <div>404 Not Found</div>
       }
     ]
   },
